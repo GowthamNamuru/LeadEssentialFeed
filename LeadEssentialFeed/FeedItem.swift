34b442c9e316dc2c2b4a68fdb44dd79f41f78b98
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-05 06:19:17 +0530

    Create the FeedItem Model

---
+++ LeadEssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  LeadEssentialFeed
+//
+//  Created by Gowtham Namuru on 05/02/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
