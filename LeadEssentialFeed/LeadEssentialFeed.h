2febdaabadf6d87a5b525196722021aa35b366c6
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-05 06:15:43 +0530

    Initial Commit

---
+++ LeadEssentialFeed/LeadEssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  LeadEssentialFeed.h
+//  LeadEssentialFeed
+//
+//  Created by Gowtham Namuru on 05/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeadEssentialFeed.
+FOUNDATION_EXPORT double LeadEssentialFeedVersionNumber;
+
+//! Project version string for LeadEssentialFeed.
+FOUNDATION_EXPORT const unsigned char LeadEssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeadEssentialFeed/PublicHeader.h>
+
+
