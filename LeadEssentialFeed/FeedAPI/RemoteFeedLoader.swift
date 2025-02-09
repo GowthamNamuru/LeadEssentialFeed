ea9d2723c20714b5d265dc66d1ecf478b1ac4792
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-09 09:21:34 +0530

    Moved default behaviour to the tests

--- LeadEssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ LeadEssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader {
         self.url = url
     }
 
-    public func load(_ completion: @escaping (Error) -> Void = {_ in }) {
+    public func load(_ completion: @escaping (Error) -> Void) {
         client.get(from: url, completion: {_ in
             completion(.connectivity)
         })
