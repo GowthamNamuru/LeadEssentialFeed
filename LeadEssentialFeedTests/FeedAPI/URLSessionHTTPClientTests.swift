72822882bf1a7d3dd10fb218957b10599e2e2778
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-16 06:36:13 +0530

    Removes redundent tests

--- LeadEssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ LeadEssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -19,15 +19,6 @@ class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_CreatesDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSPY()
-        let sut = URLSessionHTTPClient(session: session)
-
-        sut.get(from: url)
-        XCTAssertEqual(session.receivedURLS, [url])
-    }
-
     func test_getFromURL_ResumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSPY()
@@ -42,7 +33,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helper
     private class URLSessionSPY: URLSession {
-        var receivedURLS: [URL] = []
         private var stubs = [URL: URLSessionDataTask]()
 
         func stub(url: URL, task: URLSessionDataTask) {
@@ -50,7 +40,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLS.append(url)
             return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
