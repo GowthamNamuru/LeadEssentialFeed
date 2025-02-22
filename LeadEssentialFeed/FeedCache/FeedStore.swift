e588ad688c61493ff216384219db89dd1bcfbf0b
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-22 23:13:07 +0530

    Move `FeedStore` to its own file

---
+++ LeadEssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  LeadEssentialFeed
+//
+//  Created by Gowtham Namuru on 22/02/25.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
