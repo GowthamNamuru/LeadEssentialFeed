a5521efb8d39297d956977a4ae784eee192f1d1a
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-22 21:58:05 +0530

    Save command request cache deletion

--- LeadEssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ LeadEssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,25 @@
 //
 
 import XCTest
+import LeadEssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
     init(store: FeedStore) {
+        self.store = store
+    }
 
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deletCachedFeedCallCount: Int = 0
+
+    func deleteCachedFeed() {
+        deletCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -23,5 +33,24 @@ final class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
 
         XCTAssertEqual(store.deletCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deletCachedFeedCallCount, 1)
+    }
+
+    // MARK: Helper
+    func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageUrl: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
