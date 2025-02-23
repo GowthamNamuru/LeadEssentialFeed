5fb8faa857d49b56f82b26b15d025145dca9167c
gowthamnamuru <gowthamnamuri@gmail.com>
2025-02-23 10:05:07 +0530

    Add `LocalFeedItem` data transfer representation to decouple storage frameworks from `FeedItem`data models

--- LeadEssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ LeadEssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -38,10 +38,11 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let timestamp = Date()
         let (sut, store) = makeSUT(currentDate: { timestamp })
         let items = [uniqueItem(), uniqueItem()]
+        let localItems = items.map( { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageUrl: $0.imageUrl) })
         sut.save(items) {_ in }
         store.completeDeletionSuccesfully()
 
-        XCTAssertEqual(store.receivedMessage, [.deleteCacheFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessage, [.deleteCacheFeed, .insert(localItems, timestamp)])
     }
 
     func test_save_failsOnDeletionError() {
@@ -137,7 +138,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
         enum ReceivedMessage: Equatable {
             case deleteCacheFeed
-            case insert([FeedItem], Date)
+            case insert([LocalFeedItem], Date)
         }
 
 
@@ -159,7 +160,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
             deletionCompletions[index](nil)
         }
 
-        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
             insertionCompletions.append(completion)
             receivedMessage.append(.insert(items, timestamp))
         }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-### Cache Feed Use Case
+#### Cache Feed Use Case
 
-### Data:
+#### Data:
 - Feed items
 
-### Primary course (happy path):
+#### Primary course (happy path):
 1. Execute "Save Feed Items" command with above data.
 2. System deletes old cache data.
 3. System encodes feed items.
@@ -11,9 +11,9 @@
 5. System saves new cache data.
 6. System delivers success message.
 
-### Deleting error course (sad path):
+#### Deleting error course (sad path):
 1. System delivers error.
 
-### Saving error course (sad path):
+#### Saving error course (sad path):
 1. System delivers error.
 
