1165c2bb4f85c9ccd52b77ab1047894ee269bbdf
gowthamnamuru <gowthamnamuri@gmail.com>
2025-03-03 22:33:17 +0530

    Retrieving after inserting to empty cache delivers inserted values

--- LeadEssentialFeed/FeedCache/LocalFeedImage.swift
+++ LeadEssentialFeed/FeedCache/LocalFeedImage.swift
@@ -5,7 +5,7 @@
 //  Created by Gowtham Namuru on 23/02/25.
 //
 
-public struct LocalFeedImage: Equatable {
+public struct LocalFeedImage: Equatable, Codable {
     public let id: UUID
     public let description: String?
     public let location: String?
