5bd43610fdd6486785733f41941eda70c23c639c
gowthamnamuru <gowthamnamuri@gmail.com>
2025-03-27 07:07:57 +0530

    Remove `EssentialFeed` module import from `FeedViewController` file since it does not depend on any `EssentialFeed` component

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import LeadEssentialFeed
 
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     var tableModel: [FeedImageCellController] = [] {
