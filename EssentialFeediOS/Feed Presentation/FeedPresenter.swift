433f527111daa6dd53acf86dfefa862bc90fb401
gowthamnamuru <gowthamnamuri@gmail.com>
2025-04-30 08:35:18 +0530

    Add `FeedPresenter` holding feed loading presentation logic (loading state and loaded feed)

---
+++ EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,40 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Gowtham Namuru on 27/03/25.
+//
+
+import LeadEssentialFeed
+
+protocol FeedLoadingView: AnyObject {
+    func display(isLoading: Bool)
+}
+
+protocol FeedView {
+    func display(feed: [FeedImage])
+}
+
+final class FeedPresenter {
+
+    typealias Observer<T> = (T) -> Void
+
+    private var feedLoader: FeedLoader
+
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+
+    var feedView: FeedView?
+    weak var loadingView: FeedLoadingView?
+
+    func loadFeed() {
+        loadingView?.display(isLoading: true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.feedView?.display(feed: feed)
+            }
+            self?.loadingView?.display(isLoading: false)
+        }
+    }
+}
---
+++ EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -0,0 +1,38 @@
+//
+//  FeedUIComposer.swift
+//  EssentialFeediOS
+//
+//  Created by Gowtham Namuru on 24/03/25.
+//
+
+import UIKit
+import LeadEssentialFeed
+
+final public class FeedUIComposer {
+    private init() {}
+
+    public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
+        let presenter = FeedPresenter(feedLoader: feedLoader)
+        let refreshController = FeedRefreshViewController(presenter: presenter)
+        let feedController = FeedViewController(refreshController: refreshController)
+        presenter.loadingView = refreshController
+        presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
+        return feedController
+    }
+}
+
+private final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+
+    func display(feed: [FeedImage]) {
+        controller?.tableModel = feed.map { model in
+            FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+        }
+    }
+}
